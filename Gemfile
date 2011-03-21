a84b6084c02a74facbd1734cce66bed56f8cd2d0
Andrew Burgess <andrew8088@gmail.com>
2011-03-21 13:59:45 -0400

    set up mail file: francis.rb; includes db configs for dev and production

--- Gemfile
+++ Gemfile
@@ -5,8 +5,10 @@ gem "activerecord", "~>3.0"
 gem "sqlite3", "~>1.3"
 gem "haml", "~>3.0"
 
-
-
 group :development do
   gem "shotgun"
+end
+
+group :production do
+  gem "yaml"
 end
---
+++ francis.rb
@@ -0,0 +1,40 @@
+require 'sinatra/base'
+require 'activerecord'
+
+class Francis < Sinatra::Base
+
+  configure do
+    enable :sessions
+  end
+
+  configure :development do
+    ActiveRecord::Base.establish_connection adapter: "sqlite3", database: "db/dev.sqlite"
+  end
+
+  configure :production do
+    require 'yaml'
+    dbconfig = YAML.load( File.read('config/database.yml') )
+    ActiveRecord::Base.establish_connection dbconfig['production']
+  end
+
+  helpers do
+    
+  end
+
+  before "*admin*" do
+
+  end
+
+
+  # ===== Root Routes =====
+  get '/' do
+    "Hello Francis"
+  end
+
+  # ===== Admin Routes =====
+
+
+  # ===== Post Routes =====
+
+
+end
